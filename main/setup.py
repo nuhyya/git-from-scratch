d2de702f82cee9282feb16d189703945293b40c9
nuhyya <anuhyatyada@gmail.com>
2025-06-30 22:27:52 +0530

    Fix

--- main/setup.py
+++ main/setup.py
@@ -3,13 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='vctrl',
     version='0.1',
-    packages=find_packages(),  
-    py_modules=['cli'],
-    install_requires=[],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'vctrl=cli:main'
+            'vctrl = vctrl.cli:main'
         ],
     },
+    install_requires=[],
 )
 
